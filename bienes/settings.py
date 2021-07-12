481a0044a15b4c62a7b1e65aa8ad4fbe62dab60c
david <david@gmail.com>
2021-07-12 10:44:15 -0500

    api functionality

--- bienes/settings.py
+++ bienes/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "rest_framework",
+    # PROYECT APPS
+    "BienesApp"
 ]
 
 MIDDLEWARE = [
@@ -73,12 +77,26 @@ WSGI_APPLICATION = 'bienes.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+"""
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get("NAME_DB"),
+        'USER': os.environ.get("BD_USER"),
+        'PASSWORD': os.environ.get("BD_PASS"),
+        'HOST': os.environ.get("BD_HOST"),
+        'PORT': os.environ.get("BD_PORT"),
+    }
+}
+
 
 
 # Password validation
@@ -123,3 +141,12 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AUTH_USER_MODEL = 'BienesApp.CustomUser'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
--- bienes/urls.py
+++ bienes/urls.py
@@ -13,9 +13,28 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from BienesApp.models import Bienes
 from django.contrib import admin
-from django.urls import path
+from django.urls.conf import include
+from BienesApp.views import CustomUserModelViewSet, BienesModelViewSet, GetBulkBienesViewSet
+from rest_framework import routers
+from django.urls import path, re_path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+router = routers.DefaultRouter()
+router.register('Register', CustomUserModelViewSet)
+router.register('Bienes', BienesModelViewSet)
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/', include(router.urls)),
+    re_path('BienesBulk/(?P<ids>.+)/$', GetBulkBienesViewSet.as_view({'get':'list'})),
 ]
+
